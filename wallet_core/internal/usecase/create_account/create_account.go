9ce8bc08bd4b1f7d1eab45c0595990635396907c
AsonCS <acsgsa92@gmail.com>
2025-07-04 18:11:37 -0300

    entities, usecases, gateways

---
+++ wallet_core/internal/usecase/create_account/create_account.go
@@ -0,0 +1,41 @@
+package createaccount
+
+import (
+	"github.com/AsonCS/FullCycle-Microservice-Architecture/internal/entity"
+	"github.com/AsonCS/FullCycle-Microservice-Architecture/internal/gateway"
+)
+
+type CreateAccountInputDto struct {
+	ClientId string
+}
+
+type CreateAccountOutputDto struct {
+	AccountId string
+}
+
+type CreateAccountUseCase struct {
+	AccountGateway gateway.AccountGateway
+	ClientGateway  gateway.ClientGateway
+}
+
+func NewCreateAccountUseCase(a gateway.AccountGateway, c gateway.ClientGateway) *CreateAccountUseCase {
+	return &CreateAccountUseCase{
+		AccountGateway: a,
+		ClientGateway:  c,
+	}
+}
+
+func (uc *CreateAccountUseCase) Execute(input CreateAccountInputDto) (*CreateAccountOutputDto, error) {
+	client, err := uc.ClientGateway.Get(input.ClientId)
+	if err != nil {
+		return nil, err
+	}
+	account := entity.NewAccount(client)
+	err = uc.AccountGateway.Save(account)
+	if err != nil {
+		return nil, err
+	}
+	return &CreateAccountOutputDto{
+		AccountId: account.ID,
+	}, nil
+}
---
+++ wallet_core/internal/usecase/create_client/create_client.go
@@ -0,0 +1,50 @@
+package createclient
+
+import (
+	"time"
+
+	"github.com/AsonCS/FullCycle-Microservice-Architecture/internal/entity"
+	"github.com/AsonCS/FullCycle-Microservice-Architecture/internal/gateway"
+)
+
+type CreateClientInputDto struct {
+	Name  string
+	Email string
+}
+
+type CreateClientOutputDto struct {
+	ID        string
+	Name      string
+	Email     string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type CreateClientUseCase struct {
+	ClientGateway gateway.ClientGateway
+}
+
+func NewCreateClientUseCase(clientGateway gateway.ClientGateway) *CreateClientUseCase {
+	return &CreateClientUseCase{
+		ClientGateway: clientGateway,
+	}
+}
+
+func (uc *CreateClientUseCase) Execute(input CreateClientInputDto) (*CreateClientOutputDto, error) {
+	client, err := entity.NewClient(input.Name, input.Email)
+	if err != nil {
+		return nil, err
+	}
+	err = uc.ClientGateway.Save(client)
+	if err != nil {
+		return nil, err
+	}
+	output := &CreateClientOutputDto{
+		ID:        client.ID,
+		Name:      client.Name,
+		Email:     client.Email,
+		CreatedAt: client.CreatedAt,
+		UpdatedAt: client.UpdatedAt,
+	}
+	return output, nil
+}
